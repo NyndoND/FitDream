7ff7702d9e78cb06708c56acbea99fb5481536e7
renann.gomes <renannpop11@gmail.com>
2023-02-01 00:20:08 -0300

    Created Components and add typescript

--- App.tsx
+++ App.tsx
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+import theme from './src/theme/theme';
+import { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './src/pages/Login';
+import Register from './src/pages/Register';
+import Home from './src/pages/Home';
+
+
 
 export default function App() {
+  const { Navigator, Screen } = createNativeStackNavigator();
+  const [teste, setTeste] = useState('Email...');
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <Navigator
+          initialRouteName="Login"
+          screenOptions={{
+            headerShown: false,
+          }}>
+          <Screen
+            name="Login"
+            component={Login}
+          />
+          <Screen
+            name="Register"
+            component={Register}
+          />
+          <Screen
+            name="Home"
+            component={Home}
+          />
+        </Navigator>
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
